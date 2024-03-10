d23d6743bb6ffffc2166816161b9b9f75b28566b
Gabriel Germano <gabriel.lucasgermano@gmail.com>
2024-03-10 17:07:14 -0300

    :sparkles: Dados sobre as tabelas

--- testin_nparACT.R
+++ testin_nparACT.R
@@ -1,46 +1,39 @@
 install.packages('nparACT')
 library(nparACT)
+
+# Running for imputed values
 biblioPath <- "./Imputados/"
-# Lista todos os arquivos no diretório
-todos_arquivos <- list.files(biblioPath, full.names = TRUE)
 
-# Filtra os arquivos que começam com "C" seguido de números e "_predicted.txt"
-arquivos_filtrados <- grep("^C\\d+_predicted\\.txt$", todos_arquivos, value = TRUE)
+all_files <- list.files(biblioPath, full.names = TRUE)
 
-for (arquivo in arquivos_filtrados){
-  test <- read.table(arquivo, header = TRUE, sep = ";", dec = ",")
-  dados <- data.frame(DATE.TIME = test$DATA.TIME,
+result_df <- data.frame(matrix(ncol = 7, nrow = 0))
+
+for (file in all_files){
+  test <- read.table(file, header = TRUE, sep = ";", dec = ",")
+  df <- data.frame(DATE.TIME = test$DATE.TIME,
                       PIM = test$PIM)
-  dados$DATE.TIME <- as.POSIXct(dados$DATE.TIME, format = "%d/%m/%Y %H:%M:%S", tz = "UTC")
-  colnames(dados)[colnames(dados) == "DATE.TIME"] <- "DATE/TIME"
+  df$DATE.TIME <- as.POSIXct(df$DATE.TIME, format = "%d/%m/%Y %H:%M:%S", tz = "UTC")
+  colnames(df)[colnames(df) == "DATE.TIME"] <- "DATE/TIME"
   
-  results <- nparACT_base("dados", SR=1/60)
+  results <- nparACT_base("df", SR=1/60)
+  result_df <- rbind(result_df, results)
 }
+write.csv(result_df, "imputed_results.csv")
 
-file1 <- "./Imputados/C01_predicted.txt"
-test <- read.table(file1, header = TRUE, sep = ";", dec = ",")
-#dados <- data.frame(DATE.TIME = test$DATE.TIME, 
-#                    MS = test$MS,
-#                    EVENT = test$EVENT,
-#                    TEMPERATURE = test$TEMPERATURE,
-#                    EXT.TEMPERATURE = test$EXT.TEMPERATURE,
-#                    PIM = test$PIM,
-#                    TAT = test$TAT,
-#                    TATn = test$TATn,
-#                    ZCM = test$ZCM,
-#                    ZCMn = test$ZCMn,
-#                    LIGHT = test$LIGHT,
-#                    AMB.LIGHT = test$AMB.LIGHT,
-#                    RED.LIGHT = test$RED.LIGHT,
-#                    GREEN.LIGHT = test$GREEN.LIGHT,
-#                    BLUE.LIGHT = test$BLUE.LIGHT,
-#                    IR.LIGHT = test$IR.LIGHT,
-#                    STATE = test$STATE)
-dados <- data.frame(DATE.TIME = test$DATE.TIME,
-                    PIM  = test$PIM)
-dados$DATE.TIME <- as.POSIXct(dados$DATE.TIME, format = "%d/%m/%Y %H:%M:%S", tz = "UTC")
+# Running for original values
+originalPath <- "./Originais/"
+original_files <- list.files(originPath, full.names = TRUE)
 
+original_result_df <- data.frame(matrix(ncol = 7, nrow = 0))
 
-colnames(dados)[colnames(dados) == "DATE.TIME"] <- "DATE/TIME"
-
-r <- nparACT_base("dados", SR=1/60)
+for (file in original_files) {
+  txt_file <- read.table(file, header = TRUE, sep = ";", dec = ",")
+  df <- data.frame(DATE.TIME = txt_file$DATE.TIME,
+                   PIM = txt_file$PIM)
+  df$DATE.TIME <- as.POSIXct(df$DATE.TIME, format = "%d/%m/%Y %H:%M:%S", tz = "UTC")
+  
+  results <- nparACT_base("df", SR=1/60)
+  
+  original_result_df <- rbind(original_result_df, results)
+}
+write.csv(original_result_df, "original_results.csv")
